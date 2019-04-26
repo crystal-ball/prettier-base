6f26628122306d24a993183be9e38339b4707927
Dan Hedgecock <hedgecock.d@gmail.com>
2019-04-26 05:45:50 -0700

    Initialize package 🎉

---
+++ README.md
@@ -0,0 +1,19 @@
+# 🔮 Fellowship Prettier Configs
+
+## Setup
+
+*Install*
+
+```sh
+npm i -D @crystal-ball/prettier-base
+```
+
+*Reference in `package.json`*
+
+```json
+{
+  // ...
+  "prettier": "@crystal-ball/prettier-base"
+}
+```
+
---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 90,
+  proseWrap: 'always',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+
+  // --- Language overrides
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        printWidth: 80
+      }
+    }
+  ]
+}
