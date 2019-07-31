3da3ad9e4a18d67ab0b938184720f6d066e4eb68
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-07-30 17:55:04 -0700

    Update: Enable long lines in YAML files for shell commands ✨ (#15)

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ module.exports = {
 
   // --- Language overrides
   overrides: [
+    // Keep markdown files very compact for readability
     {
       files: '*.md',
       options: {
         printWidth: 80,
+      },
+    },
+    // Allow really long lines in YAML files because it's often easier to read
+    // shell commands when they're not broken into multiple lines awkwardly
+    {
+      files: ['*.yml', '*.yaml'],
+      options: {
+        printWidth: 140,
       },
     },
   ],
